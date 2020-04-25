be3c3531c1df61a3f9f114fa06c502b1daf72cf5
Patrick Borje <borje.pb@gmail.com>
2020-04-25 17:08:17 +0800

    Updates

---
+++ precache-manifest.0f3aae1f63adb24682414b1192268cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b6b4151ae056bcce90bad52a6b9609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce65b7b8ff87ffb5b737",
+    "url": "/static/css/main.b55afae5.chunk.css"
+  },
+  {
+    "revision": "d2a0c5e5571ae1614d4b",
+    "url": "/static/js/2.65268254.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.65268254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce65b7b8ff87ffb5b737",
+    "url": "/static/js/main.0983c72d.chunk.js"
+  },
+  {
+    "revision": "e67a23b3e0990bb5759c",
+    "url": "/static/js/runtime-main.76f7158a.js"
+  }
+]);
